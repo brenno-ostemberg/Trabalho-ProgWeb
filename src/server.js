6aafb85ade466aa15fd58926359268c07e124111
brenno-ostemberg <brenno.ostemberg.dev@gmail.com>
2024-11-23 17:17:29 -0400

    fix(server.js): corrigindo caminho do HTML e views

--- src/server.js
+++ src/server.js
@@ -3,16 +3,25 @@ const cors = require('cors');
 const path = require('path');
 const mainController = require('./controllers/mainController'); 
 
+// Instância do Express
 const app = express();
 
 // Configuração de Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Servindo arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Configuração de Views para o HTML
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
 // Configuração de Rotas
 app.use('/', mainController);
 
+// Configuração de Porta
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
